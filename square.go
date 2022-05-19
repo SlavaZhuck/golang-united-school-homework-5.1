3a59546f529b2414b3c2f70619cb0546fe0c0600
Viachaslau_Zhuk <Zhukviachaslau@gmail.com>
2022-05-19 22:13:53 +0300

    init commit

---
+++ square.go
@@ -0,0 +1,13 @@
+package square
+
+// package main
+
+// func main() {
+// 	//	m := "+1-2 -3 -5"
+// 	m := "-a-2"
+// 	var a string
+// 	fmt.Println(m)
+// 	a, e := StringSum(m)
+// 	fmt.Println("string", a)
+// 	fmt.Println("error: ", e)
+// }
