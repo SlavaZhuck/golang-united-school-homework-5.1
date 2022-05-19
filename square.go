aec96916c7a840ffadc617f698bc1baee7953ce6
Viachaslau_Zhuk <Zhukviachaslau@gmail.com>
2022-05-19 22:35:37 +0300

    added methods

--- square.go
+++ square.go
@@ -1,5 +1,27 @@
 package square
 
+type Point struct {
+	x, y int
+}
+
+type Square struct {
+	start Point
+	side  uint
+}
+
+func (r *Square) Area() uint {
+	return r.side * r.side
+}
+
+func (r Square) Perimeter() uint {
+	return 4 * r.side
+}
+
+func (r Square) End() Point {
+	out := Point{r.start.x + int(r.side/2), r.start.y + int(r.side/2)}
+	return out
+}
+
 // package main
 
 // func main() {
